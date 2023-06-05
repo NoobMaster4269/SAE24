1b663c42c175c209852bd5631dd4df4b03ffa189
NoobMaster4269 <134286591+NoobMaster4269@users.noreply.github.com>
2023-06-06 00:05:46 +0200

    Add files via upload

---
+++ dns.py
@@ -0,0 +1,17 @@
+from scapy.all import *
+trames = rdpcap('ftp-total.pcapng')
+
+for paquet in trames: 
+    if paquet.haslayer("UDP"):
+        print("ENTÊTE UDP")
+        print(f"Port source : {paquet[UDP].sport}")
+        print(f"Port destination : {paquet[UDP].dport}")
+
+    elif paquet.haslayer("DNS"):
+        print("ENTÊTE DNS : ")
+        print(f"Qr : {paquet[DNS].qr}")
+        print(f"Opcode : {paquet[DNS].opcode}")
+        print(f" Nom : {paquet[DNS].qd.qname}")
+        print(f"Type : {paquet[DNS].qd.qtype}")
+
+sniff(filter='upd port 53', prn=trames, store=0)
