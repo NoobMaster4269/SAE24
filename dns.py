f714c0d78d3e891669de2c9501c9ff0ab68ab94b
NoobMaster4269 <134286591+NoobMaster4269@users.noreply.github.com>
2023-06-06 10:33:35 +0200

    dns.py

--- dns.py
+++ dns.py
@@ -2,12 +2,8 @@ from scapy.all import *
 trames = rdpcap('ftp-total.pcapng')
 
 for paquet in trames: 
-    if paquet.haslayer("UDP"):
-        print("ENTÊTE UDP")
-        print(f"Port source : {paquet[UDP].sport}")
-        print(f"Port destination : {paquet[UDP].dport}")
-
-    elif paquet.haslayer("DNS"):
+    if paquet.haslayer("UDP") and paquet[UDP].dport == 53:
+        paquet.haslayer("DNS"):
         print("ENTÊTE DNS : ")
         print(f"Qr : {paquet[DNS].qr}")
         print(f"Opcode : {paquet[DNS].opcode}")
